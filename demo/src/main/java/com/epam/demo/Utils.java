53535209972d17d8034e8133ec97e0f070f2a0cb
Taalay Joldoshev <gmoftj@gmail.com>
2024-01-22 17:32:57 +0600

    Done

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,24 @@
 package com.epam.demo;
 
+import com.epam.utils.StringUtils;
+
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-       //magic happens here
+        boolean broken = false;
+
+        if (args == null || args.isEmpty()) {
+            broken = true;
+        } else {
+            for (String arg : args) {
+                if (!StringUtils.isPositiveNumber(arg)) {
+                    broken = true;
+                    break;
+                }
+            }
+        }
+
+        return !broken;
     }
 }
